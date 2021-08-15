d075848a5f9544088506101b7e40105b7e6e26ac
Hideo Yasumoto <hideo54@hideo54.com>
2021-08-16 00:26:10 +0900

    Init config borrowed from Candidates2021

---
+++ README.md
@@ -0,0 +1,11 @@
+# @hideo54/esling-config
+
+## How to use
+
+Run `npm i -D @hideo54/esling-config` and extends it like:
+
+```
+{
+    "extends": "@hideo54
+}
+```
---
+++ index.js
@@ -0,0 +1,53 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'next',
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+    ],
+    rules: {
+        indent: [
+            'warn',
+            4,
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        quotes: [
+            'error',
+            'single',
+        ],
+        semi: [
+            'error',
+            'always',
+        ],
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/ban-ts-comment': 'warn',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+};
